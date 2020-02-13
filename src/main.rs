3fe411c17d061c42a49679c493349b6c1aa3cd86
mikelma <mikelma7@gmail.com>
2020-02-13 20:45:00 +0100

    Now reading settings from config file

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,23 @@ use std::time::Duration;
 use ostruka::{handle_user, instance};
 use instance::{Instance, Page};
 
+mod config;
+use config::Config;
+
 #[tokio::main]
 async fn main() {
-    // TODO: Read from config page 
-    let username = "mike";
-    let password = "9d4e1e23bd5b727046a9e3b4b7db57bd8d6ee684"; // TODO: Encrypt and sign
-    let addr: SocketAddr = "127.0.0.1:9999".parse().unwrap();
+    
+    // Read the config file and deserialize it into Config
+    let config = Config::new("config.toml").unwrap();
+
+    let addr: SocketAddr = config.server_address.parse().unwrap();
 
     // Create a client and log in
-    let (mut client, tx) = Client::log_in(username, 
-                                                password, 
-                                                addr).await
+    let (mut client, tx) = Client::log_in(&config.user, 
+                                          &config.password, 
+                                          addr).await
         .unwrap_or_else(|err| {
-            eprintln!("Unable to log in as {} in {}. Error: {}", username, addr, err);
+            eprintln!("Unable to log in as {} in {}. Error: {}", config.user, addr, err);
             process::exit(1);
         });
 
@@ -50,7 +54,7 @@ async fn main() {
     
     // Launch the user handler
     tokio::spawn(async move {
-        if let Err(err) = handle_user(username, tx, instance_clone).await {
+        if let Err(err) = handle_user(&config.user, tx, instance_clone).await {
             panic!("Fatal error: {}", err);
         }
     });
@@ -60,7 +64,7 @@ async fn main() {
             Message::ToSend(cmd) => {
                 if let Err(err) = client.send_cmd(&cmd).await {
                     instance.lock().await
-                        .add_err(&format!("SERVER: {}", err))
+                        .add_err(&format!("[✗] SERVER: {}", err))
                         .unwrap();
                 }
             },
